1560b467565619b4fe84907e554eca3a917701c8
shu0115 <s.matsumoto0115@gmail.com>
2018-02-15 15:41:27 +0900

    Fix transactions create

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,19 +1,21 @@
 class TransactionsController < ApplicationController
   permits :hash_key, :sender_address, :input_amount
 
-  def generate(blockchain_id, sender_address, receiver_address, output_amount)
+  def create(sender_address, receiver_address, output_amount)
+    blockchain = Blockchain.find_by(unique_key: Blockchain::UNIQUE_KEY)
+
     ActiveRecord::Base.transaction do
       # ブロック作成
       block = Block.new
-      block.blockchain_id = blockchain_id
-      block.index         = Block.where(blockchain_id: blockchain_id).maximum(:index).to_i + 1
-      block.previous_hash = Block.where(blockchain_id: blockchain_id).order(index: :desc).first&.hash_key
+      block.blockchain_id = blockchain.id
+      block.index         = Block.where(blockchain_id: blockchain.id).maximum(:index).to_i + 1
+      block.previous_hash = Block.where(blockchain_id: blockchain.id).order(index: :desc).first&.hash_key
       block.generate_at   = Time.current
       block.save!
 
       # トランザクション作成
       transaction = TradeTransaction.new
-      transaction.blockchain_id  = blockchain_id
+      transaction.blockchain_id  = blockchain.id
       transaction.block_id       = block.id
       transaction.hash_key       = TradeTransaction.generate_unique_key
       transaction.sender_address = sender_address
@@ -23,7 +25,7 @@ class TransactionsController < ApplicationController
 
       # 送信先トランザクション作成
       transaction_output = TransactionOutput.new
-      transaction_output.blockchain_id        = blockchain_id
+      transaction_output.blockchain_id        = blockchain.id
       transaction_output.block_id             = block.id
       transaction_output.trade_transaction_id = transaction.id
       transaction_output.receiver_address     = receiver_address
@@ -33,7 +35,7 @@ class TransactionsController < ApplicationController
 
       # 残高を自身のアドレスへ送金
       transaction_output = TransactionOutput.new
-      transaction_output.blockchain_id        = blockchain_id
+      transaction_output.blockchain_id        = blockchain.id
       transaction_output.block_id             = block.id
       transaction_output.trade_transaction_id = transaction.id
       transaction_output.receiver_address     = sender_address
--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,13 @@ Rails.application.routes.draw do
   # root to: 'blockchains#index'
   root to: 'blocks#index'
 
-  resources :nodes, only: [:index, :create]
+  resources :nodes,        only: [:index, :create]
+  resources :blocks,       only: [:index]
+  resources :transactions, only: [:create]
 
   # resources :blockchains, only: [:index, :new, :create, :show] do
   #   post 'transactions/generate', to: 'transactions#generate'
   # end
-
-  resources :blocks, only: [:index] do
-    post 'transactions/generate', to: 'transactions#generate'
-  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
